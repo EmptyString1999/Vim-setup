b13dd828a914cbeb18344e037193091f59591615
Ethan Price <32579301+EmptyString1999@users.noreply.github.com>
2025-05-17 16:34:24 +0100

    Add: <leader>r to swap windows

--- lua/emptystring/lazy/chadtree.lua
+++ lua/emptystring/lazy/chadtree.lua
@@ -4,6 +4,30 @@ return {
 	init = function()
 		-- Optional: Set keybindings to toggle CHADTree
 		vim.keymap.set("n", "<leader>e", "<cmd>CHADopen<cr>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>r", function()
+		local current_win = vim.api.nvim_get_current_win()
+      local is_chadtree = vim.bo.filetype == "CHADTree"
+
+      if is_chadtree then
+        -- Return to previous window if in CHADtree
+        vim.cmd("wincmd p")
+      else
+        -- Find existing CHADtree window or open it
+        local found_chadtree = false
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.api.nvim_buf_get_option(buf, "filetype") == "CHADTree" then
+            vim.api.nvim_set_current_win(win)
+            found_chadtree = true
+            break
+          end
+        end
+
+        if not found_chadtree then
+          vim.cmd("CHADopen")
+        end
+      end
+    end, { noremap = true, silent = true, desc = "Swap focus between tree/file" })	
 		local chadtree_settings = {
 			view = {
 				window_options = {
